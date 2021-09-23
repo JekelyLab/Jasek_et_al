debab246e834814ff0b60a5557e3072507174e51
Gaspar Jekely <g.jekely@exeter.ac.uk>
2021-09-23 19:31:03 +0100

    MUSlong: changed annotation

--- Revision_separate_syn_and_desmo_connectors.R
+++ Revision_separate_syn_and_desmo_connectors.R
@@ -7,7 +7,7 @@ library(natverse)
 source("~/R/conn.R")
 
 #read in dorsal MUSlong cells with connectors
-MUSlong = nlapply(read.neurons.catmaid("^celltype_non_neuronal75$", pid=11, 
+MUSlong = nlapply(read.neurons.catmaid("^longitudinal muscle$", pid=11, 
                                        fetch.annotations = T), function(x) smooth_neuron(x, sigma=6000))
 attributes(MUSlong)
 class(MUSlong)
@@ -27,5 +27,5 @@ str(MUSlong_conn1)
 plot3d(MUSlong_conn1$x, MUSlong_conn1$y, MUSlong_conn1$z, add = TRUE, col = 'red', size=6, alpha=0.5)
 plot3d(MUSlong_conn3$x, MUSlong_conn3$y, MUSlong_conn3$z, add = TRUE, col = 'cyan', size=6, alpha=0.5)
 
-
+MUSlong_conn3$x
 
