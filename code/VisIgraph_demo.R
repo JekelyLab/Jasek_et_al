0cae62f73bea4b63c75242f9890903559c34bbe1
Gaspar Jekely <g.jekely@exeter.ac.uk>
2022-02-18 12:43:54 +0000

    Desmo_network: VisIgraph plotting code added

--- code/VisIgraph_demo.R
+++ code/VisIgraph_demo.R
@@ -1,178 +1,108 @@
-require(igraph)
-library(natverse)
+
+#load packages
+{
+library(igraph)
 library(visNetwork)
 library(ggplot2)
 library(ggraph)
-
+library(rgexf)
+library(RColorBrewer)
+}
  
-conn <- source("~/R/conn.R")
-#for larger calls we need to use http/1, see https://www.gitmemory.com/issue/natverse/rcatmaid/158/641537466
-#for this we configure to http/1.1
-conn_http1 = catmaid_login(conn=conn, config=httr::config(ssl_verifypeer=0, http_version=1))
 
 #read adjacency matrix
-
-desmo_conn <- read.csv("/Users/gj274/OneDrive\ -\ University\ of\ Exeter/Paper/Muscles/code/Jasek_et_al/data/adjacency_matrix_desmosomal_connectome_CATMAID.csv",
-         sep=",")
+desmo_conn <- read.csv("data/adjacency_matrix_desmosomal_connectome_CATMAID.csv", sep=",")
 desmo_conn <- as.matrix(desmo_conn)
 
-
-#alernatively, retrieve neurons from catmaid
-connectome_IN = read.neurons.catmaid("^eye_circuit$", pid=11, fetch.annotations = T, conn=conn_http1)
-
-skids <- attr(connectome_IN,"df")$skid
-#or
-skids <- names(connectome_IN)
-N_neurons <- length(skids)
-connectivity_IN <- catmaid_get_connectors_between(pre=skids, 
-                                                  post=skids, pid=11)
-connectivity_IN
-N_neurons
-# use table() to cross-tabulate number of connections between skids
-connectivity_IN_table = table(connectivity_IN[,1:2])
-connectivity_IN_table[1:10,1:10]
-dim(connectivity_IN_table)
-# create empty connectivity matrix
-conn_mat = matrix(0,nrow=N_neurons,ncol=N_neurons)
-# name rows and cols for skids
-colnames(conn_mat) = skids
-rownames(conn_mat) = skids
-
-# populate connectivity matrix based on np_conn_table
-for (x in rownames(connectivity_IN_table)){
-  for (y in colnames(connectivity_IN_table)){
-    conn_mat[x,y] = connectivity_IN_table[x,y]
-  }
-}
-
-#change skids to neuron names
-neuron_names <- catmaid_get_neuronnames(skids, pid = 11, conn = NULL)
-colnames(conn_mat) = neuron_names
-rownames(conn_mat) = neuron_names
-
-conn_mat
 #with the make_graph function of igraph we turn it into a graph (input is the list of edge pairs)
-Conn_graph <- graph_from_adjacency_matrix(
-  conn_mat, mode = c("directed"),
-  weighted = TRUE, diag = TRUE)
-#or
 Conn_graph <- graph_from_adjacency_matrix(
   desmo_conn[,2:dim(desmo_conn)[2]],
   mode = c("undirected"), weighted = TRUE, diag = TRUE)
 
-#calculate node weights
-weights=strength(Conn_graph, vids = V(Conn_graph), mode = c("all"),
-                 loops = TRUE)
-degree=degree(Conn_graph, v = V(Conn_graph), mode = c("all"), loops = TRUE, normalized = FALSE)
-
-# passing some info  
-g <- Conn_graph
-
-#V(g)$color <- c("green")
-V(g)$value <- degree*5
-V(g)$label.cex = 1
-V(g)$label.color = "black"
-E(g)$value <- E(g)$weight/10
-E(g)[ E(g)$weight > 5 ]$color <- "grey50"
-
 #create a new graph only from the largest connected component
 #check connected componenets
-cl <- components(g)
+cl <- components(Conn_graph)
 g_graph_largest = induced_subgraph(g, which(cl$membership == 1))
 g_graph_largest
 
-library(rgexf)
+#calculate node weights
+weights=strength(g_graph_largest, vids = V(g_graph_largest), mode = c("all"),
+                 loops = TRUE)
+degree=degree(g_graph_largest, v = V(g_graph_largest), mode = c("all"), loops = TRUE, normalized = FALSE)
+
+# passing some info  
+V(g_graph_largest)$value <- degree
+E(g_graph_largest)$value <- E(g_graph_largest)$weight
+
+
+
 # Serialise the graph to Gephi format
-gexf_data <- rgexf::igraph.to.gexf(g_graph_largest)
+#gexf_data <- rgexf::igraph.to.gexf(g_graph_largest)
 # Write the network into a gexf (Gephi) file
-write.gexf(gexf_data, output = "g_graph_largest.gexf")
+#write.gexf(gexf_data, output = "data/g_graph_largest.gexf")
 #do force field layout and Leiden modularity in Gephi
 
-#reimport gephi file
-gexf_data <- read.gexf("Full_desmosomal_graph.gexf")
-#plot as interactive Gephi javascript page
-plot(gexf_data)
+#reimport gephi file after layout in gephi
+#gexf_data <- read.gexf("data/g_graph_largest_layout.gexf")
+
 #convert to igraph
-g_graph_largest <- rgexf::gexf.to.igraph(gexf_data)
+#g_graph_largest <- rgexf::gexf.to.igraph(gexf_data)
 
-visIgraph(g_graph_largest)
-
-# Compute clusters of similarity from the graph data
-com <- cluster_louvain(g_graph_largest)
+# Compute Leiden communitites from the graph data
+com <- cluster_leiden(g_graph_largest,
+                      objective_function = "modularity",
+                      weights = NULL,
+                      resolution_parameter = 0.3,
+                      beta = 0.01,
+                      n_iterations = 100)
 #number of communities
 N_communities <- max(com$membership)
 N_communities
-col=hcl.colors(max(com$membership), "Set 2")
-
-# If vertices are in the same cluster, give them the same colour
-V(g_graph_largest)$color.border <- "black"
-V(g_graph_largest)$color.background <- col[com$membership]
-#define font size individually for nodes
-V(g_graph_largest)$label.cex <- V(g_graph_largest)$value/30
-E(g_graph_largest)$weight
-#change vertex shape based on type
-#V(g_graph_largest)[V(g_graph_largest)$type == 1]$shape <- "square"
-#V(g_graph_largest)[V(g_graph_largest)$type == 0]$shape <- "circle"
-
-#color edges based on vertex color
-edge.start <- ends(g_graph_largest, es=E(g_graph_largest),names=F)[,1]
-ec <- V(g_graph_largest)$color.background[edge.start]
-E(g_graph_largest)$color <- ec
 
 
-gexf_data$nodesVizAtt$position$x
- 
-# The DrL layout algorthm uses a random number generator. Setting
-# the seed to the same number produces the same layout each time.
-{set.seed(42)
-  # Comput the DrL graph layout
-  l = layout_with_drl(g_graph_largest, options=list(init.attraction=0, 
-                                                    simmer.attraction=0,expansion.attraction=0,
-                                                    liquid.attraction=0,cooldown.attraction=0,
-                                                    cooldown.temperature=0,
-                                                    simmer.temperature=600, simmer.iterations=10000
-                                                    )) 
-}
+#generate coluors for the different communities
+col=list()
+Okabe_Ito <- c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", 
+               "#CC79A7", "#000000")
+col[1:7] <- Okabe_Ito[1:7]
+col[8:max(com$membership)]=rev(brewer.pal(max(com$membership)-7, "Paired"))
 
-#define the coordinates based on the drl layout
-coords <- as.matrix(data.frame(x = l[,1], y = l[,2], stringsAsFactors = FALSE))
-#alternatively, get coordinates from imported gephi file
+#get coordinates from imported gephi file
 coords <- as.matrix(data.frame(x = gexf_data$nodesVizAtt$position$x, y = gexf_data$nodesVizAtt$position$y, stringsAsFactors = FALSE))
 
-ec
-#V(g_graph_largest)$value <- sqrt(V(g_graph_largest)$value)
-# Plot the graph on-screen
-plot.igraph(g_graph_largest, layout=coords, frame=FALSE, vertex.size=weights/20, vertex.label=NA, 
-     vertex.frame.color='grey25', vertex.label.family = 'sans', edge.width=E(g_graph_largest)$weight/5, 
-     edge.arrow.size=E(g_graph_largest)$weight/4, edge.curved=0.1, 
-     add=FALSE, edge.color=ec
-     #mark.groups = c(5:12),
-     #mark.border = 'red',
-     #mark.col = 'red',
-)
+## convert to VisNetwork-list
+Conn_graph.visn <- toVisNetworkData(g_graph_largest)
+## copy column "weight" to new column "value" in list "edges"
+Conn_graph.visn$edges$value <- Conn_graph.visn$edges$weight
+
+#assign node membership
+Conn_graph.visn$nodes$group <- com$membership
+#colour by group
+Conn_graph.visn$nodes$color <- unlist(lapply(Conn_graph.visn$nodes$group, function(x) col[Conn_graph.visn$nodes$group[x]]))
+
+Conn_graph.visn$nodes$size <- sqrt(V(g_graph_largest)$value)*10
 
 
+#plot the same graph by using the coordinates from the gephi layout
+visNet <- visNetwork(Conn_graph.visn$nodes,Conn_graph.visn$edges) %>% 
+  visIgraphLayout(layout = "layout_nicely", physics = TRUE, randomSeed = 42) %>%
+  visNodes(borderWidth=0.3, 
+           color = list(background=Conn_graph.visn$nodes$color, border='black'),
+           opacity=1,
+           shape='dot',
+           physics=TRUE,
+           font=list(color='black', size=0),
+           scaling = list(label=list(enabled=TRUE, min=16, max=28))) %>%
+  visEdges(smooth = list(type = 'curvedCW', roundness=0.1),
+           scaling=list(min=2, max=8),
+           color = list(inherit=TRUE, opacity=0.4)) %>%
+  visPhysics(solver = "forceAtlas2Based",
+           forceAtlas2Based = list(gravitationalConstant = -1000, 
+                     theta=0.1,
+                     springLength=30,
+                     avoidOverlap=1,
+                     centralGravity=0.5,
+                     springConstant=0.5,
+                     damping=1))
 
-
-#define node size, shape and color
-V(g_graph_largest)$shape <- "circle"
-V(g_graph_largest)$value <- degree*50
-#define font size individually for nodes
-V(g_graph_largest)$label.cex <- V(g_graph_largest)$value/2240
-#visualise with visNetwork
-visIgraph(g_graph_largest) %>% 
-#   visIgraphLayout(layout = "layout_nicely", physics = TRUE, randomSeed = 42) %>%
-#   visPhysics(solver = "forceAtlas2Based",
-#             forceAtlas2Based = list(gravitationalConstant = -100, avoidOverlap=1)) %>%
-   visEdges(smooth = list(type = 'curvedCW', roundness=0), color='grey') %>%
-   visNodes(borderWidth=0.3, color = list(highlight = "yellow")) %>%
-   visOptions(highlightNearest = TRUE) %>%
-   visInteraction(navigationButtons = TRUE,
-                 dragNodes = TRUE, dragView = TRUE,
-                 zoomView = TRUE) %>%
-  visIgraphLayout(layout = "layout.norm", layoutMatrix = coords)
-  
-  
-
-
-
+visNet
