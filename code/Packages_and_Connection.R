08c264fb9359b0f455ab940723cd50dedfc024e3
Gaspar Jekely <g.jekely@exeter.ac.uk>
2022-07-18 11:12:00 +0100

    Packages and Connection: file added to be sourced

---
+++ code/Packages_and_Connection.R
@@ -0,0 +1,21 @@
+#packages to source and details of CATMAID connection
+
+rm(list = ls(all.names = TRUE)) #will clear all objects includes hidden objects.
+gc() #free up memrory and report the memory usage.
+Sys.setenv('R_MAX_VSIZE'=8000000000)
+
+# load nat and all associated packages, incl catmaid
+library(natverse)
+library(magick)
+library(nat)
+options(nat.plotengine = 'rgl')
+require("graphics")
+
+# catmaid connection, needs username, password AND token - weird!
+conn <- source("~/R/conn.R")
+#for larger calls we need to use http/1, see https://www.gitmemory.com/issue/natverse/rcatmaid/158/641537466
+#for this we configure to http/1.1
+conn_http1 = catmaid_login(conn=conn, config=httr::config(ssl_verifypeer=0, http_version=1))
+
+
+
