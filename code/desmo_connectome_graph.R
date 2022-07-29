b4dc4265b3657d251bb24c289114701df330ed17
Gaspar Jekely <g.jekely@exeter.ac.uk>
2022-07-29 16:51:13 +0200

    desmo_connectome_graph: updated coordintae assignments, leiden clustering, annotation of nodes from CATMAID and saving - the output can be used in other scripts to plot the connectome graph with the gephi layout #read the saved visNetwork file from supplements/ conn_graph.visn <- readRDS("supplements/desmo_connectome_graph.rds")

--- code/desmo_connectome_graph.R
+++ code/desmo_connectome_graph.R
@@ -50,12 +50,12 @@ E(g.largest)$weight = 1
 desmo_conn_graph <- igraph::simplify(g.largest, remove.loops = FALSE,
             edge.attr.comb = list(weight = "sum", function(x) length(x)) )
 
-E(desmo_conn_graph)
+E(desmo_conn_graph)$weight
 
 #clustering with Leiden algorithm
 partition <- leiden(desmo_conn_graph, weights = E(desmo_conn_graph)$weight, 
-                    partition_type = "ModularityVertexPartition",
-                    resolution_parameter = 1,
+                    partition_type = "RBConfigurationVertexPartition",
+                    resolution_parameter = 0.25,
                     n_iterations = -1, seed = 42)
 max(partition)
 
@@ -72,13 +72,20 @@ desmo_conn_graph.tb <- desmo_conn_graph %>%
   mutate(partition = partition) %>%
   mutate(color = col[partition])
 
-
 # export graph for gephi to do force field layout -------------------------
 
 # Serialise the graph to Gephi format
 gexf_data <- rgexf::igraph.to.gexf(desmo_conn_graph.tb)
 # Write the network into a gexf (Gephi) file
 write.gexf(gexf_data, output = "data/full_desmo_connectome_graph.gexf")
+
+#Force field clustering was carried out with the Force Atlas tool in Gephi (0.9.2)
+#The inertia was set to 0.1, repulsion strength was 35, 
+#attraction strength was 10, maximum displacement was 5, 
+#gravity was 50, speed was 5 and the 'attraction distribution' option was selected. 
+#The 'auto stabilise function' was off. Towards the end of the clustering 
+#the 'adjust by sizes' option was also selected. 
+#To prevent node overlap, we then run the 'Noverlap' function. 
 
 # read gexf file ----------------------------------------------------------
 
@@ -90,9 +97,11 @@ conn_gexf <- rgexf::read.gexf("data/full_desmo_connectome_force_layout.gexf")
 
 #get coordinates from imported gephi file
 coords <- as_tibble(x = conn_gexf$nodesVizAtt$position$x) %>%
-  mutate(x = value) %>%
+  rename('x' = value) %>%
   mutate(y = conn_gexf$nodesVizAtt$position$y) %>%
-  mutate(skid = conn_gexf$nodes$id)
+  mutate(skid = as.numeric(conn_gexf$nodes$id))
+coords
+
 #sort by skid
 x_coord <-  as.list(arrange(coords, desc(skid)) %>%
                       select(x) )
@@ -101,13 +110,12 @@ y_coord <-  as.list(arrange(coords, desc(skid)) %>%
 
 #assign coordinates from imported gephi graph to original CATMAID graph
 desmo_conn_graph.tb <- activate(desmo_conn_graph.tb, nodes) %>%
-  arrange(desc(name)) %>%  #sort by skid to match coordinate list
+  arrange(desc(as.numeric(name))) %>%  #sort by skid to match coordinate list
   mutate(x = unlist(x_coord),  #add coordinates
          y = unlist(y_coord))
 
-
 # search for names and annotations --------------------------------------------------
-
+{
 #nodes are named with the skid, use these to get neuron names from CATMAID
 #get names with the tidygraph pull function
 skids <- pull(desmo_conn_graph.tb, name)
@@ -125,7 +133,8 @@ length(cell_names)
 #check for the presence of the annotations and add the annotation to the type_of_cell list
 type_of_cell <- list()
 annot_to_search <- c("muscle", "epithelia_cell", "basal lamina", 
-                     "ciliated cell", "glia cell")
+                     "acicula", "chaeta", "circumacicular",
+                     "circumchaetal", "ciliated cell", "glia cell")
 for(i in seq_along(skids)){
   annot <- catmaid_get_annotations_for_skeletons(skids=skids[i], pid = 11)
   for (j in seq_along(annot_to_search)) {
@@ -169,84 +178,33 @@ for(i in seq_along(skids)){
   print (i)
 }
 
-
+}
 
 #add type of cell to node$group (can be used for visualisation)
 desmo_conn_graph.tb <- desmo_conn_graph.tb %>%
   mutate(group = unlist(type_of_cell)) %>%
   mutate(class = unlist(type_of_cell)) %>%
-  mutate(skid = name) %>%
-  mutate(name = unlist(cell_names)) %>%
+  mutate(CATMAID_name = unlist(cell_names)) %>%
   mutate(segment = unlist(segment_of_cell)) %>%
   mutate(side = unlist(side_of_cell))
-
 
 # VisNetwork conversion ---------------------------------------------------
 
 {
-  #convert to visNetwork graph
-  conn_graph.visn <- toVisNetworkData(as.igraph(desmo_conn_graph.tb))  
+#convert to visNetwork graph
+conn_graph.visn <- toVisNetworkData(as.igraph(desmo_conn_graph.tb))  
   
-  ## copy column "weight" to new column "value" in list "edges"
-  conn_graph.visn$edges$value <- conn_graph.visn$edges$weight
+## copy column "weight" to new column "value" in list "edges"
+conn_graph.visn$edges$value <- conn_graph.visn$edges$weight
   
-  #color will be assigned by group (type of cell)
-  #visGroups - $nodes$color takes precedence
+#color will be assigned by group (type of cell)
+#visGroups - $nodes$color takes precedence
   
-  #save vis graph with annotations as R data file and txt file printed with dput() to get an exact copy
-  saveRDS(conn_graph.visn, "supplements/desmo_connectome_graph.rds")
-  writeLines(capture.output(dput(conn_graph.visn)), "supplements/desmo_connectome_graph.txt")
+#save vis graph with annotations as R data file and txt file printed with dput() to get an exact copy
+saveRDS(conn_graph.visn, "supplements/desmo_connectome_graph.rds")
+writeLines(capture.output(dput(conn_graph.visn)), "supplements/desmo_connectome_graph.txt")
 }
 
-# plot graph with coordinates from gephi ----------------------------------
-
-{
-  coords <- matrix(c(conn_graph.visn$nodes$x, conn_graph.visn$nodes$y), ncol=2)
-  
-  visNet2 <- visNetwork(conn_graph.visn$nodes,conn_graph.visn$edges) %>% 
-    visIgraphLayout(layout = "layout.norm", layoutMatrix = coords) %>%
-    visEdges(smooth = list(type = 'curvedCW', roundness=0),
-             scaling=list(min=1, max=25),
-             color = list(inherit=TRUE, opacity=0.7),
-             arrows = list(to = list(enabled = TRUE, 
-                                     scaleFactor = 0.5, type = 'arrow'))) %>%
-    visNodes(borderWidth=0.3, 
-             color = list(border='black'),
-             opacity = 1, 
-             font = list(size = 20)) %>%
-    visOptions(highlightNearest = list(enabled=TRUE, degree=1, 
-                                       algorithm = 'hierarchical',labelOnly=FALSE), 
-               width = 500, height = 500, autoResize = FALSE) %>%
-    visInteraction(dragNodes = TRUE, dragView = TRUE,
-                   zoomView = TRUE, hover=TRUE,
-                   multiselect=TRUE) %>%
-    addFontAwesome() %>%
-    visEvents(selectNode  = "function(e){
-            var label_info = this.body.data.nodes.get({
-            fields: ['label', 'label_long'],
-            filter: function (item) {
-            return item.id === e.node
-            },
-            returnType :'Array'
-            });
-            this.body.data.nodes.update({id: e.node, label : label_info[0].label_long, label_long : label_info[0].label});
-            }") %>% 
-    visEvents(blurNode  = "function(e){
-            var label_info = this.body.data.nodes.get({
-            fields: ['label', 'label_long'],
-            filter: function (item) {
-            return item.id === e.node
-            },
-            returnType :'Array'
-            });
-            this.body.data.nodes.update({id: e.node, label : label_info[0].label_long, label_long : label_info[0].label});
-  }")
-  
-  #save as html
-  saveNetwork(visNet2, "pictures/Full_desmo_connectome_modules.html", selfcontained = TRUE)
-  webshot2::webshot(url="pictures/Full_desmo_connectome_modules.html",
-                    file="pictures/Full_desmo_connectome_modules_webshot.png",
-                    vwidth = 550, vheight = 550, #define the size of the browser window
-                    cliprect = c(50, 60, 480, 480), zoom=8, delay = 5)
-}
+#read the saved visNetwork file from supplements/
+conn_graph.visn <- readRDS("supplements/desmo_connectome_graph.rds")
 
