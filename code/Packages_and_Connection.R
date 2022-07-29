ef81b8eecca7a3d5a9e64c527e1b864825e51199
Gaspar Jekely <g.jekely@exeter.ac.uk>
2022-07-29 18:11:09 +0200

    packages: added background plotting and reading of background volumes and skeletons

--- code/Packages_and_Connection.R
+++ code/Packages_and_Connection.R
@@ -52,4 +52,38 @@ display.brewer.all(colorblindFriendly = TRUE)
 brewer12 <- brewer.pal(12, 'Paired')
 
 
+#read volumes
+#These volumes are 3D structures in the animal's body and provide background 
+#for reconstructed neurons or help create images
+{
+outline <- catmaid_get_volume(1, rval = c("mesh3d", "catmaidmesh", "raw"),
+                                invertFaces = T, conn = NULL, pid = 11)
+yolk <- catmaid_get_volume(4, rval = c("mesh3d", "catmaidmesh", "raw"),
+                             invertFaces = T, conn = NULL, pid = 11)
+acicula <-   nlapply(read.neurons.catmaid("^acicula$", pid=11),
+                       function(x) smooth_neuron(x, sigma=6000))
+#these four dots are the most extreme points of the volume, adding them to the 3d view solves the problem with automatic zooming and movement of the field shown
+bounding_dots = nlapply(read.neurons.catmaid("^bounding_dots$", pid=11),
+                          function(x) smooth_neuron(x, sigma=6000))
+scalebar_50um_anterior = read.neurons.catmaid("^scalebar_50um_anterior$", pid=11)
+scalebar_50um_ventral = read.neurons.catmaid("^scalebar_50um_ventral$", pid=11)
+
+}
+
+#plotting function for ventral view with yolk and acicula
+plot_background_ventral <- function(x){
+  nopen3d() # opens a pannable 3d window
+  par3d(windowRect = c(20, 30, 600, 800)) #to define the size of the rgl window
+  nview3d("ventral", extramat=rotationMatrix(0, 1, 0, 0))
+  plot3d(bounding_dots, WithConnectors = F, WithNodes = F, soma=F, lwd=1,
+         rev = FALSE, fixup = F, add=T, forceClipregion = F, alpha=1,
+         col="white") 
+  plot3d(yolk, WithConnectors = F, WithNodes = F, soma=F, lwd=2,
+         rev = FALSE, fixup = F, add=T, forceClipregion = F, alpha=0.05,
+         col="#E2E2E2") 
+  plot3d(acicula, WithConnectors = F, WithNodes = F, soma=T, lwd=2,
+         rev = FALSE, fixup = F, add=T, forceClipregion = F, alpha=1,
+         col="grey70")
+  par3d(zoom=0.48)
+}
 
