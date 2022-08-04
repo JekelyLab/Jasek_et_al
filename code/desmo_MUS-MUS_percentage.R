c002a7f0acbb609aeb268b02ea6abfaf8c63fe4c
Sanja Jasek <jasek.sanja@gmail.com>
2022-08-04 16:21:31 +0100

    Muscle-to-muscle desmosome percentage: fix bug, add also basal lamina count.
    Counting basal lamina to determine what percentage are hemidesmosomes.

--- code/desmo_MUS-MUS_percentage.R
+++ code/desmo_MUS-MUS_percentage.R
@@ -8,6 +8,7 @@ desmo_count_MUS = 0
 desmo_count_MUS_same = 0
 desmo_count_MUS_other = 0
 desmo_count_other = 0
+desmo_count_lamina = 0
 
 for (i in (1:length(all_desmo_connectors$connectors))) {
   print(i)
@@ -15,29 +16,43 @@ for (i in (1:length(all_desmo_connectors$connectors))) {
   skid2 <- all_desmo_connectors$partners[[i]][[2]][[3]]
   skid1annotations <- catmaid_get_annotations_for_skeletons(skid1, pid=11)
   skid2annotations <- catmaid_get_annotations_for_skeletons(skid2, pid=11)
-  if ("muscle" %in% skid1annotations$annotation & "muscle" %in% skid2annotations$annotation ) {
+  mus1 <- skid1annotations$annotation[grepl("^muscle$", skid1annotations$annotation)]
+  mus2 <- skid2annotations$annotation[grepl("^muscle$", skid2annotations$annotation)]
+  lamina1 <- skid1annotations$annotation[grepl("^basal lamina$", skid1annotations$annotation)]
+  lamina2 <- skid2annotations$annotation[grepl("^basal lamina$", skid2annotations$annotation)]
+  if (length(mus1)>0 & length(mus2)>0) {
     desmo_count_MUS = desmo_count_MUS + 1
-    print(paste("skids ", skid1, " and ", skid2))
+    print(paste("skids ", skid1, " and ", skid2, "are muscles"))
     celltype <- skid1annotations$annotation[grepl("celltype_non_neuronal.", skid1annotations$annotation)]
-    if (length(celltype)>1) {
+    print(celltype)
+    if (length(celltype)>0) {
       if (celltype %in% skid2annotations$annotation) {
         desmo_count_MUS_same = desmo_count_MUS_same + 1
+        print("same muscle type")
       }
       else {
         desmo_count_MUS_other = desmo_count_MUS_other + 1
+        print("different muscle types")
       }
     }
     else {
       desmo_count_MUS_other = desmo_count_MUS_other + 1
     }
   }
+  else if (length(lamina1)>0 | length(lamina2)>0) { 
+    print(paste("skids", skid1, "or", skid2, "are basal lamina"))
+    desmo_count_lamina = desmo_count_lamina + 1
+  }
   else {
     desmo_count_other = desmo_count_other + 1
   }
 }
 
+
+percentage_hemidesmo <- desmo_count_lamina / length(all_desmo_connectors$connectors)
+
 # percentage of desmosoems which are muscle to muscle
-percentage_MUS_desmo <- desmo_count_MUS / (desmo_count_other + desmo_count_MUS)
+percentage_MUS_desmo <- desmo_count_MUS / length(all_desmo_connectors$connectors)
 
 # percentage of muscle to muscle desmosoems which are between same muscle types
 percentage_MUS_same_desmo <- desmo_count_MUS_same / (desmo_count_MUS_other + desmo_count_MUS_same)
