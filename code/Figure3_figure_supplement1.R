2fc61b43c191b608d75047bdf3dc4af00f4258af
Gaspar Jekely <g.jekely@exeter.ac.uk>
2022-08-08 15:02:16 +0100

    Fig3_fig_suppl: assembly of fig, upload of source_data as html

--- code/Figure3_figure_supplement1.R
+++ code/Figure3_figure_supplement1.R
@@ -1,10 +1,12 @@
-```{r}
+
+source("code/Packages_and_Connection.R")
+
 #read the saved visNetwork file from source_data/ ------------------------------
 
 conn_grouped_graph.visn <- readRDS("source_data/Figure3_figure_supplement1_source_data1.rds")
-```
 
-```{r}
+
+
 # plot grouped graph ----------------------------------
 
 #overwrite group value (partition) with side value or other value (for colouring)
@@ -23,9 +25,6 @@ conn_grouped_graph.visn$nodes$value <-
 conn_grouped_graph.visn$nodes$color <- c()
 Reds <- brewer.pal(9, 'Reds')
 
-```
-
-```{r}
 # shorten names and assign names to node labels --------------------------------
 
 #shorten names after _ and \s
@@ -35,9 +34,6 @@ conn_grouped_graph.visn$nodes$CATMAID_name  <- sub("\\s.*$", "", conn_grouped_gr
 #assign name to label
 conn_grouped_graph.visn$nodes$label <-  conn_grouped_graph.visn$nodes$CATMAID_name
 
-```
-
-```{r}
 # generate visNet graph ---------------------------
 
 visNet <- visNetwork(conn_grouped_graph.visn$nodes, 
@@ -80,19 +76,49 @@ visNet <- visNetwork(conn_grouped_graph.visn$nodes,
                                      opacity=0.8) 
 
 visNet
-```
 
-```{r}
 # save graph ---------------------------
 
 #save as html
-saveNetwork(visNet, "pictures/Grouped_desmo_connectome.html", selfcontained = TRUE)
+saveNetwork(visNet, "source_data/Figure3_figure_supplement1_source_data2.html", selfcontained = TRUE)
 #create png snapshot
-webshot2::webshot(url="pictures/Grouped_desmo_connectome.html",
+webshot2::webshot(url="source_data/Figure3_figure_supplement1_source_data2.html",
                     file="pictures/Grouped_desmo_connectome.png",
                     vwidth = 1500, vheight = 1500, #define the size of the browser window
                     cliprect = c(50, 60, 1500, 1500), zoom=5, delay = 2)
-```
 
 
 
+# assemble figure ---------------------------------------------------------
+
+{
+panel_ch <- ggdraw() + draw_image(readPNG("pictures/Desmo_grouped_chaeta.png")) +
+  draw_label("partners of chaetae", x = 0.5, y = 0.99, size = 9)
+panel_ERcirc <- ggdraw() + draw_image(readPNG("pictures/Desmo_grouped_ERcirc.png")) +
+  draw_label("partners of chaetal follicle 1", x = 0.5, y = 0.99, size = 9)
+panel_noERcirc <- ggdraw() + draw_image(readPNG("pictures/Desmo_grouped_neERcirc.png")) +
+  draw_label("partners of chaetal follicle 2", x = 0.5, y = 0.99, size = 9)
+panel_ac <- ggdraw() + draw_image(readPNG("pictures/Desmo_grouped_ac.png")) +
+  draw_label("partners of aciculae", x = 0.5, y = 0.99, size = 9)
+panel_all <- ggdraw() + draw_image(readPNG("pictures/Desmo_grouped.png")) +
+  draw_label("grouped desmosomal connectome", x = 0.5, y = 0.99, size = 9)
+panel_circumac <- ggdraw() + draw_image(readPNG("pictures/Desmo_grouped_circumac.png")) +
+  draw_label("partners of circumacicular", x = 0.5, y = 0.99, size = 9)
+
+layout <-
+"ABC
+DEF"
+
+Fig3_fig_suppl1 <- panel_all + panel_ac + panel_circumac +
+  panel_ch + panel_ERcirc + panel_noERcirc +
+  plot_layout(design = layout) +
+  plot_annotation(tag_levels = 'A') & 
+  theme(plot.tag = element_text(size = 12, face='plain'))
+
+ggsave("figures/Figure3_figure_supplement1.pdf", limitsize = FALSE, 
+       units = c("px"), Fig3_fig_suppl1, width = 2500, height = 1800)
+
+ggsave("figures/Figure3_figure_supplement1.png", limitsize = FALSE, 
+       units = c("px"), Fig3_fig_suppl1, width = 2500, height = 1800, bg = 'white')
+
+}  
