930c0e38e651b18e7180ab73affe64c29d7696c9
Gaspar Jekely <g.jekely@exeter.ac.uk>
2022-08-19 19:11:32 +0100

    Video7.R: new code, video corresponding to Fig6_fig_suppl5 showing desmosomes between two cell groups, various examples and full-body rotation. Rotation is done with rotationMatrix, this also works with many skeletons with alpha<1, even if slow. Add text first, then remove text with rgl.pop after finding ids of text objects by parsing the output of ids3d() to continue with other examples from Fig6_fig_suppl5

---
+++ code/Video7.R
@@ -0,0 +1,126 @@
+#code for Video7 of the Jasek et al 2021 desmosomal connectome paper
+#Gaspar Jekely
+
+source("code/Packages_and_Connection.R")
+
+#read the tibble with desmosome ids, xyz coordinates and partners 1 and 2
+desmo_with_partners <- read_csv("data/desmo_with_partners.csv")
+
+#read skids of connectome neurons to plot as anatomical ref
+connectome_skids <- catmaid_skids("^connectome_neuron$", pid=11)
+#read a random subset of connectome skeletons only for speed up
+connectome <- nlapply(read.neurons.catmaid(sample(connectome_skids, 300), pid=11),
+                      function(x) smooth_neuron(x, sigma = 6000))
+
+#define function to read two cell classes and plot desmosomes between them and 
+#skeletons from the two cell classes that are connected
+plot_two_cell_types_with_desmo <- function(annotation1, annotation2, 
+                                           color1, color2, 
+                                           soma1, soma2){
+  annot1 <- paste("annotation:", annotation1, sep = "")
+  annot2 <- paste("annotation:", annotation2, sep = "")
+  celltype1_skids <- catmaid_skids(annot1, pid=11)
+  celltype2_skids <- catmaid_skids(annot2, pid=11)
+  
+  celltype1_obP_des <- desmo_with_partners %>%
+    filter(partner1 %in% celltype1_skids & partner2 %in% celltype2_skids |
+             partner2 %in% celltype1_skids & partner1 %in% celltype2_skids)
+  
+  skids1 <- celltype1_obP_des %>%
+    filter(partner1 %in% celltype1_skids) %>%
+    select(partner1) %>%
+    pull()
+  
+  skids2 <- celltype1_obP_des %>%
+    filter(partner2 %in% celltype1_skids) %>%
+    select(partner2) %>%
+    pull()
+  
+  celltype1_restricted_skids <- c(skids1, skids2)
+  
+  skids1b <- celltype1_obP_des %>%
+    filter(partner1 %in% celltype2_skids) %>%
+    select(partner1) %>%
+    pull()
+  
+  skids2b <- celltype1_obP_des %>%
+    filter(partner2 %in% celltype2_skids) %>%
+    select(partner2) %>%
+    pull()
+  
+  celltype2_restricted_skids <- c(skids1b, skids2b)
+  
+  #get xyz coordinates
+  x <- unlist(celltype1_obP_des %>% select(x))
+  y <- unlist(celltype1_obP_des %>% select(y))
+  z <- unlist(celltype1_obP_des %>% select(z))
+  
+  #read skeletons based on skids
+  celltype1 <- nlapply(read.neurons.catmaid(unique(celltype1_restricted_skids), pid=11),
+                      function(x) smooth_neuron(x, sigma=6000))
+  celltype2 <- nlapply(read.neurons.catmaid(unique(celltype2_restricted_skids), pid=11),
+                         function(x) smooth_neuron(x, sigma=6000))
+  
+  #plot the shared desmosomes by xyz coordinates 
+  plot3d(x, y, z, add = TRUE, col='red', size=6, alpha=1)
+  
+  #plot the two cell groups
+  plot3d(celltype1, add = TRUE, soma = soma1, col=color1, lw=3, alpha=0.4)
+  plot3d(celltype2, add = TRUE, soma = soma2, col=color2, lw=3, alpha=0.5)
+
+}
+
+#background plot
+plot_background_ventral_no_acic()
+
+#plot MUSlongD and MUSobP_notD
+plot_two_cell_types_with_desmo("celltype_non_neuronal75", 
+                               "celltype_non_neuronal54",
+                               Okabe_Ito[1], Okabe_Ito[5], 
+                               FALSE, FALSE)
+texts3d(49000,76000, 88000, text = "MUSlongD", col='black', cex = 2)
+texts3d(29000,76000, 105000, text = "MUSobP-notD", col='black', cex = 2)
+
+#plot MUSobP_notD and EC-circumchaetal
+plot_two_cell_types_with_desmo("celltype_non_neuronal28", 
+                               "celltype_non_neuronal54",
+                               Okabe_Ito[7], Okabe_Ito[5],
+                               TRUE, FALSE)
+texts3d(33000,76000, 122000, text = "EC-circumchaetal", col='black', cex = 2)
+
+#plot outline and connectome as anatomical ref
+plot3d(connectome, add=T, alpha=0.1, lw = 1, col = "black") 
+plot3d(outline, add = T, alpha = 0.02, col = "#E2E2E2") 
+
+#snapshot with text
+rgl.snapshot("videos/Video7_desmo_partners_A.png")
+
+#get the ids of text objects from the rgl window with ids3d()
+text_ids <- ids3d() %>% 
+  as_tibble() %>%
+  filter(type == 'text') %>%
+  select(id) %>%
+  pull()
+
+#remove text
+rgl.pop(type = 'shapes', id = text_ids)
+
+#full rotation with extramat - also works with many skeletons with alpha<1
+for (i in 100:200){
+  nview3d("ventral", extramat=rotationMatrix((i-100)*2*pi/100, 0, 0, 1))
+  #save a snapshot in the working directory
+  rgl.snapshot(paste("videos/Video7_desmo_partners_spin", i, ".png", sep = ""))
+}
+
+
+close3d()
+
+#read png files and write video
+library(av)
+av::av_encode_video(paste('videos/', list.files("videos", '*.png'), sep = ""), 
+                    framerate = 5,
+                    output = 'videos/Video_desmo_partners.mp4')
+
+
+file.remove(paste('videos/', list.files("videos", '*.png'), sep = ""))
+#delete individual video frames
